4b2a4f2ef316bb8506d3aa124ec950e33f5dc3f1
Gourav Shah <gs@initcron.org>
2015-10-22 14:52:29 +0530

    added base cookbook

--- roles/base.rb
+++ roles/base.rb
@@ -2,8 +2,8 @@ name "base"
 description "Base Server Role"
 run_list  "recipe[ntp]", "recipe[chef-client]", "recipe[chef-client::delete_validation]",  "recipe[motd]", "recipe[users]"
 default_attributes({
-  "chef_client" => { "interval" => '60' , 
-  	                 "splay"    => 20
+  "chef_client" => { "interval" => '300' , 
+  	                 "splay"    => '60'
   }, 
   "ntp" => {
     "servers" => ['time0.int.example.org', 'time1.int.example.org']
